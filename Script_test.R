6868e8192b3f39473e06520f796e557e2d51f13a
jdauteri <jdauteri@gmu.edu>
2019-02-18 11:13:12 -0500

    geocoding nonsense

--- Script_test.R
+++ Script_test.R
@@ -1,5 +1,6 @@
 #load scripts 
 library(dplyr)
+library(ggplot2)
 library(readr)
 url <- "https://github.com/jdauteri/HackberryGit/blob/master/song_requests.csv"
 if (!file.exists("song_requests.csv")) {
@@ -10,12 +11,13 @@ all_mail <- read_csv("song_requests.csv")
 
 #arranged by request
 all_mail %>% 
-  select (Sender_Location, Request) %>% 
-  arrange(desc(Request))
+  select (ID, Sender_Location, Request) %>% 
+  arrange(desc(Request)) %>% 
+  View
 
 #arranged by sender location
 all_mail %>% 
-  select (Sender_Location, Request) %>% 
+  select (ID, Sender_Location, Request) %>% 
   arrange(desc(Sender_Location))
 
 #count frequency of each sender location
@@ -53,31 +55,105 @@ top_songs <- all_mail %>%
   filter(n >= 4) %>% 
   arrange(desc(n)) 
 
+#top 10 songs
+all_mail %>% 
+  count(Request) %>% 
+  head(10) %>% 
+  arrange(desc(n)) %>% 
+  View
+
+#top songs with visualization
+top_songs$Request <- factor(top_songs$Request, levels = top_songs$Request[order(-top_songs$n)])
+top_songs %>% 
+  head(5) %>% 
+  ggplot(aes(x = Request, y = n)) +
+  geom_col(fill="beige", colour="gray") +
+  coord_flip() +
+  theme_minimal() +
+  labs(title="Top 5 Song Requests", y = "Count", x = "Song Title")
+
+
+#songs per city with visualization--can create a sonic profile of local listening and how that changed across the region
+city <- all_mail %>% 
+  filter(Sender_Location == "Iowa, LA") %>% 
+  count(Request) %>% 
+  arrange(desc(n))
+city$Request <- factor(city$Request, levels = city$Request[order(-city$n)])
+city %>% 
+  head(5) %>% 
+  ggplot(aes(x = Request, y = n)) +
+  geom_col(fill="beige", colour="gray") +
+  coord_flip() +
+  theme_minimal() +
+  labs(title="Iowa, LA Top 5 Songs", y = "Count", x = "Song Title")
+
+#top songs recipient
+city <- all_mail %>% 
+  filter(Recipient_Location == "Sulphur, LA") %>% 
+  count(Request) %>% 
+  arrange(desc(n))
+city$Request <- factor(city$Request, levels = city$Request[order(-city$n)])
+city %>% 
+  head(5) %>% 
+  ggplot(aes(x = Request, y = n)) +
+  geom_col(fill="beige", colour="gray") +
+  coord_flip() +
+  theme_minimal() +
+  labs(title="", y = "Count", x = "Song Title")
+
+#songs per city Hackberry (3 part-make city, then factor, then viz)
+city <- all_mail %>% 
+  filter(Sender_Location == "Hackberry, LA") %>% 
+  count(Request) %>% 
+  arrange(desc(n))
+city$Request <- factor(city$Request, levels = city$Request[order(-city$n)])
+city %>% 
+  head(5) %>% 
+  ggplot(aes(x = Request, y = n)) +
+  geom_col() 
+
+
+
+
 #songs for war
-french <- all_mail %>% 
+war <- all_mail %>% 
   select(Request, Request_reason) %>% 
   filter(Request_reason == "War") %>% 
   count(Request) %>% 
-  arrange(desc(n)) 
+  arrange(desc(n))
+
+#plot top 5 from french
+war %>% 
+  head(5) %>% 
+  ggplot(aes(x = Request, y = n)) +
+  geom_col()
+ 
 
 #songs by language--check unclear/unspecified
 all_mail %>% 
-  count(Language_Request)
+  count(Language_Request) %>% 
+  ggplot(aes(x = Language_Request, y = n)) +
+  geom_col() 
 
-#French songs frequencies
+#French songs frequencies with visualization
 all_mail %>% 
   select(Request, Language_Request, Sender_Location) %>% 
   filter(Language_Request == "French") %>% 
   count(Request) %>% 
-  arrange(desc(n))
+  arrange(desc(n)) %>% 
+  head(5) %>% 
+  ggplot(aes(x = Request, y = n)) +
+  geom_col() 
 
-#top request from each city
+
+#top request from each city--not sure if this is working
 all_mail %>% 
   select(Sender_Location, Request) %>% 
   group_by(Sender_Location) %>% 
   top_n(1, Request) %>% 
   View
 
+#not sure what this is doing
 all_mail %>% 
   select(Recipient_Location, Request) %>% 
   group_by(Recipient_Location) %>% 
@@ -85,7 +161,14 @@ all_mail %>%
   top_n(2, Request) %>% 
   View
 
+#could do this grouping by city and some kind of frequency chart?
+all_mail %>% 
+  filter(Sender_Location == "Iowa, LA") %>% 
+  group_by(Language_Request) %>% 
+  summarize(total_meetings = n())
 
+
+##vis factor script city$Request <- factor(city$Request, levels = city$Request[order(-city$n)])
 
 
 
--- test.R
+++ test.R
@@ -16,3 +16,61 @@ all_mail %>%
 ggplot(all_mail, aes(x = Request)) +
   geom_bar()
 
+##mapping tutorial http://eriqande.github.io/rep-res-web/lectures/making-maps-with-R.html
+
+#attmpt at mapping
+library(ggplot2)
+library(ggmap)
+library(maps)
+library(mapdata)
+
+#create state data
+usa <- map_data("usa")
+states <- map_data("state")
+dim(states)
+head(states)
+tail(states)
+louisiana <- subset(states, region == "louisiana")
+
+#get county data 
+head(louisiana)
+counties <- map_data("county")
+la_county <- subset(counties, region == "louisiana")
+head(la_county)
+
+#plot LA
+ggplot(data = louisiana) + 
+  geom_polygon(aes(x = long, y = lat, group = group), fill = "beige", color = "black") + 
+  coord_fixed(1.3)
+
+#plot LA no grid
+la_base <- ggplot(data = louisiana, mapping = aes(x = long, y = lat, group = group)) + 
+  coord_fixed(1.3) + 
+  geom_polygon(color = "black", fill = "beige")
+la_base + theme_nothing()
+
+#plot LA counties
+la_base + theme_nothing() + 
+  geom_polygon(data = la_county, fill = NA, color = "black") +
+  geom_polygon(color = "black", fill = NA) 
+
+
+library(ggmap)
+map <- get_map(location = 'Europe', zoom = 4)
+get_openstreetmap
+
+geocoinstall.packages("googleAuthR")
+de("Lake Charles, LA")
+install.packages("googleAuthR")
+
+geocode("the white house") %>% 
+  View
+
+set.seed(500)
+df <- round(data.frame(
+  x = jitter(rep(-95.36, 50), amount = .3),
+  y = jitter(rep( 29.76, 50), amount = .3)
+), digits = 2)
+map <- get_googlemap('houston', markers = df, path = df, scale = 2)
+ggmap(map, extent = 'device')
+
